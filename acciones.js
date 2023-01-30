d352b32958d82831e861b6a09abcf5e565e9991f
DanielJara8 <122488757+DanielJara8@users.noreply.github.com>
2023-01-29 23:26:35 -0300

    cuarto commit 29/1
    creación de código JavaScript

--- acciones.js
+++ acciones.js
@@ -1,3 +1,20 @@
-function ocultar(){
-    document.getElementById('contacto').style.display='none';
-}
+/*prueba de enlace de documentos*/
+console.log("Prueba de carga");
+
+/*Agregado de eventos de botón*/
+
+        window.addEventListener("DOMContentLoaded", (event) => {
+            console.log("DOM cargado");
+
+            const menuBtn = document.getElementById("menu");
+            const nav = document.querySelector("header nav");
+
+            menuBtn.addEventListener("click", (event) =>{
+                menuBtn.classList.toggle("salir");
+                nav.classList.toggle("visualizar");
+            });
+           console.log("DOM cargado");
+            
+        });
+
+    
